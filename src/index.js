46adb83b88672d4d68ca4674c60704e4216c44c2
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-05-18 00:45:09 +0600

    Lecture 06: React Components and Props Core Concept - React Tutorial Bangla Series

--- src/index.js
+++ src/index.js
@@ -1,12 +1,87 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 
-const index = 0;
-const element = (
-  <h1 className='heading' tabIndex={index}>
-    <span className='text'>Hello {new Date().toLocaleTimeString()}</span>
-    <img src='' />
-  </h1>
-);
+// // example: 01
+// function Clock() {
+//   return (
+//     <h1 className='heading'>
+//       <span className='text'>Hello {new Date().toLocaleTimeString()}</span>
+//     </h1>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(Clock());
+
+// // example: 02
+// function Clock() {
+//   return (
+//     <h1 className='heading'>
+//       <span className='text'>Hello {new Date().toLocaleTimeString()}</span>
+//     </h1>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Clock />);
+
+// // example: 03: sending props from external
+// function Clock({ locale }) {
+//   return (
+//     <h1 className='heading'>
+//       <span className='text'>Hello {new Date().toLocaleTimeString(locale)}</span>
+//     </h1>
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Clock locale='bn-BD' />);
+
+// //example: 04: class component
+// class Clock {
+//   print() {
+//     return (
+//       <h1 className='heading'>
+//         <span className='text'>Hello {new Date().toLocaleTimeString()}</span>
+//       </h1>
+//     );
+//   }
+// }
+
+// const ClockComponent = new Clock();
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(ClockComponent.print());
+
+// //example: 05: class component with react component
+// class Clock extends React.Component {
+//   render() {
+//     return (
+//       <h1 className='heading'>
+//         <span className='text'>Hello {new Date().toLocaleTimeString()}</span>
+//       </h1>
+//     );
+//   }
+// }
+
+// const ClockComponent = new Clock();
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Clock />);
+
+//example: 05: class component with react component passing props to element
+class Clock extends React.Component {
+  render() {
+    // here donot chenge any props value , otherwise this will go inside infinite loop
+    return (
+      <h1 className='heading'>
+        <span className='text'>
+          Hello {this.props.children}
+          {new Date().toLocaleTimeString(this.props.locale)}
+        </span>
+      </h1>
+    );
+  }
+}
+
+const ClockComponent = new Clock();
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(element);
+root.render(<Clock locale='bn-BD'>test</Clock>);
